92490232b415be18c487dbdf9b4f4efa82ed5dd6
peterkariukimutuura <kariukipeter30@gmail.com>
2017-12-01 18:20:38 +0300

    add Junit Tests

--- src/test/java/hangmanTest.java
+++ src/test/java/hangmanTest.java
@@ -1,2 +1,21 @@
 import org.junit.*;
 import static org.junit.Assert.*;
+
+public class hangmanTest{
+
+
+    @Test
+    public static void CheckUserInput(String Username){
+        
+        Boolean status;
+        if (Username.equals(null)||Username.equals("")) {
+            System.out.println("Username is required"); 
+        }else{
+            System.out.println(Username + " was added");
+        }
+        assertEquals("Name", Username);
+        
+    }
+
+
+}
